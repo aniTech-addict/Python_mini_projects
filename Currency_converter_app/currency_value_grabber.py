690e488432c433aa733cb62bb6c66615d0944410
Brad <kirti7666anand15@gmail.com>
2025-06-11 23:57:08 +0530

    Sucessfull Api calls implemented

--- Currency_converter_app/currency_value_grabber.py
+++ Currency_converter_app/currency_value_grabber.py
@@ -7,27 +7,72 @@ from supported_country_codes import country_codes
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
+    
+    
+    
+def currency_value(home_country,foreign_country):
+    """_summary_
 
-def get_exchange_value(country_name):
+    Args:
+        home_country (_type_): The country name of the base currency
+        foreign_country (_type_): The country name of the counter currency
+
+    Returns:
+        int: returns the counter currency value 
+    """
+    conversion_rates  = get_conversion_rates(home_country)
+    
+    foreign_country_code = find_country_code(foreign_country)
+    
+    return conversion_rates[foreign_country_code]
+    
+    
+    
+def find_country_code(country_name):
+    """_summary_
+
+    Args:
+        country_name (_type_): The country name for which the code is to be found
+
+    Returns:
+        str: The country code for the given country name
+    """
     if country_name.upper() not in country_codes:
         print("-------------------------------------------------\n"
               "Unsupported Country, Please recheck the spelling or Enter another Country Name"
               "\n-----------------------------------------------")
-        return
+        
+    return country_codes[country_name.upper()] 
+
+
+
+
+def get_conversion_rates(home_country):
+    """_Grab the conversion rates for the given home country
+
+    Args:
+        home_country (str): The country name of the base currency
+
+    Returns:
+        dict: A dictionary containing the conversion rates
+    """
     
-    country_code = country_codes[country_name.upper()] 
+    country_code = find_country_code(home_country)
     base_link = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{country_code}"
 
     try:
         response = requests.get(url=base_link)
         response.raise_for_status()
-        return response.json()
-    
+        
+        return response.json()['conversion_rates']
+
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err} - Status Code: {response.status_code if 'response' in locals() else 'N/A'}")
     except requests.exceptions.RequestException as req_err:
         print(f"An error occurred while making the requests: {req_err}")
-    return None
+
+
+
 
 def main():
     print("This file is not to be called")
