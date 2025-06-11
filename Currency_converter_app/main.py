daf3dd91c3b03b3323b74fc072be872d526759bf
Brad <kirti7666anand15@gmail.com>
2025-06-11 23:57:42 +0530

    base app ready

--- Currency_converter_app/main.py
+++ Currency_converter_app/main.py
@@ -1,14 +1,29 @@
+from typing import Counter
 from dotenv import load_dotenv
-from currency_value_grabber import get_exchange_value 
 import json
 import os
 
+import currency_value_grabber as CV
+
 def main():
+    """
+    1. Prints a welcome message.
+    2. Asks the user to input an amount and the country names of the base and foreign currencies.
+    3. Calls the currency_value function from the CV module to get the conversion rate.
+    4. Prints the converted amount by multiplying the input amount with the conversion rate.
+    
+    """
     
     print("\n~~ Currency converter app ~~\n")
+    
+    base_currency_amount = int(input("Enter the Amount :").replace(',', ''))
+    
+    base_country_name = input("Enter the Base Currency country name :").strip()
+    counter_country_name = input("Enter foreign Country name :").strip()
 
-    country_name = input("Enter Country Name")
-    conversion_values = get_exchange_value(country_name)
+    counter_currency_value = CV.currency_value(base_country_name,counter_country_name)
+    
+    print(f"Value after Conversion = {base_currency_amount* counter_currency_value}")
     
 
 
