1ac29a7d32e7dbd2d0fb3e4097449cf0ffeeca43
Brad <kirti7666anand15@gmail.com>
2025-06-15 05:57:21 +0530

    Refactor main function to include weather data interpretation and summary display

--- prj_5_Weather-App-Using-API/main.py
+++ prj_5_Weather-App-Using-API/main.py
@@ -1,12 +1,14 @@
-from handle_weather_api import get_weather_info
+from handle_weather_api import get_weather_info, interpret_weather_data
 
 def main():
     city_name = input("Enter City name-> ")
     state_name = input("Enter State name-> ")
     
-    get_weather_info(city_name,state_name)
+    weather_data = get_weather_info(city_name,state_name)
+    weather_summary = interpret_weather_data(weather_data)
+    print(weather_summary)
 
-    
+        
 if __name__ == "__main__":
     print("\n~~ Weather App ~~\n")    
     main()
